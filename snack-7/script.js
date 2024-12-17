41c7f88d887d791cb83394927905c85e7bd7ab71
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-17 12:42:12 +0100

    feat: snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//recuper lo studente con id 2 con (.id === 2)
+const student = students.find(student => student.id === 2);
+
+console.log(student);
